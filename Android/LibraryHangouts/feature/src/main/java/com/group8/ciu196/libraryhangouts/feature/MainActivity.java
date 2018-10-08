b71161d47965279f46e1e10e0a3351044cebebb1
Rasmusgit <rasan@student.chalmers.se>
2018-10-08 10:58:47 +0200

    estimote

--- Android/LibraryHangouts/feature/src/main/java/com/group8/ciu196/libraryhangouts/feature/MainActivity.java
+++ Android/LibraryHangouts/feature/src/main/java/com/group8/ciu196/libraryhangouts/feature/MainActivity.java
@@ -2,12 +2,95 @@ package com.group8.ciu196.libraryhangouts.feature;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+
+import com.estimote.coresdk.common.requirements.SystemRequirementsChecker;
+import com.estimote.coresdk.observation.region.beacon.BeaconRegion;
+import com.estimote.coresdk.service.BeaconManager;
+import com.estimote.mgmtsdk.feature.settings.api.Beacon;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BeaconManager beaconManager;
+    private BeaconRegion region;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        beaconManager = new BeaconManager(this);
+        region = new BeaconRegion("ranged region",
+                UUID.fromString("B9407F30-F5F8-466E-AFF9-25556B57FE6D"), null, null);
+
+       
+
+
     }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        SystemRequirementsChecker.checkWithDefaultDialogs(this);
+
+        beaconManager.connect(new BeaconManager.ServiceReadyCallback() {
+            @Override
+            public void onServiceReady() {
+                beaconManager.startRanging(region);
+            }
+        });
+    }
+
+
+    @Override
+    protected void onPause() {
+        beaconManager.stopRanging(region);
+
+        super.onPause();
+    }
+
+
+    private static final Map<String, List<String>> PLACES_BY_BEACONS;
+
+    // TODO: replace "<major>:<minor>" strings to match your own beacons.
+    static {
+        Map<String, List<String>> placesByBeacons = new HashMap<>();
+        placesByBeacons.put("22504:48827", new ArrayList<String>() {{
+            add("Heavenly Sandwiches");
+            // read as: "Heavenly Sandwiches" is closest
+            // to the beacon with major 22504 and minor 48827
+            add("Green & Green Salads");
+            // "Green & Green Salads" is the next closest
+            add("Mini Panini");
+            // "Mini Panini" is the furthest away
+        }});
+        placesByBeacons.put("648:12", new ArrayList<String>() {{
+            add("Mini Panini");
+            add("Green & Green Salads");
+            add("Heavenly Sandwiches");
+        }});
+        PLACES_BY_BEACONS = Collections.unmodifiableMap(placesByBeacons);
+    }
+
+    private List<String> placesNearBeacon(Beacon beacon) {
+        String beaconKey = String.format("%d:%d", region.getMajor(), region.getMinor());
+        if (PLACES_BY_BEACONS.containsKey(beaconKey)) {
+            return PLACES_BY_BEACONS.get(beaconKey);
+        }
+        return Collections.emptyList();
+    }
+
+
+
 }
